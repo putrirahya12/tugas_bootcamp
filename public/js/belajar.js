01c8dd602b405507228c5772141b3c2745286bf2
NorIndah <claranor100@gmail.com>
2019-05-27 12:00:08 +0800

    belajar js

--- public/js/belajar.js
+++ public/js/belajar.js
@@ -1,5 +1,13 @@
 $(function() {
 
-    alert('wow!');
+    $('#tombol-tambah').on('click', function() {
+        alert('wow!');
+    });
+
+    // saat tombol sembunyi di klik
+    $('#tombol-sembunyi').on('click', function() {
+        // sembunyikan class informasi
+        $('.informasi').hide('slow');
+    });
 
 });
--- resources/views/belajar-js.blade.php
+++ resources/views/belajar-js.blade.php
@@ -4,9 +4,40 @@
 
 @section('content')
     
-    <p>View nya belum kepake dulu!.</p>
+    <p class="informasi">View nya belum kepake dulu!.</p>
+
+    <a href="#" class="btn btn-success" id="tombol-tambah">
+        Tambah Data
+    </a>
+
+    <a href="#" class="btn btn-primary" id="tombol-sembunyi">
+        Sembunyikan Info
+    </a>
+
+    <div><span>coba target saya 1!</span></div>
+    <div><span>coba target saya 2!</span></div>
+    <div><span>coba target saya 3!</span></div>
     
 @endsection
+
+@push('css')
+    <style>
+        #tombol-tambah {
+            /* display: none; */
+        }
+
+        .red {
+            color: red;
+            font-size: 20px;
+            font-weight: bold;
+        }
+        .blue {
+            color: blue;
+            font-style: italic;
+            text-decoration: underline;
+        }
+    </style>
+@endpush
 
 @push('js')
     <script src="{{ url('js/belajar.js') }}"></script>
